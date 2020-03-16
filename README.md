08b6b51dee61af27ef368eace7c0710a11e98ccd
Guliqiang <guliqiang@sensetime.com>
2020-03-16 20:54:25 +0800

    polymer cli run add

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # sense-ui
 🖖SenseUI is a Web Components &amp; Custom Elements for Future Web Applications 
+
+
+```shell
+npm install -g polymer-cli
+polymer serve
+```
---
+++ src/js/my-ele.js
@@ -0,0 +1,10 @@
+import {html, render} from 'lit-html';
+
+// This is a lit-html template function. It returns a lit-html template.
+const helloTemplate = (name) => html`<div>Hello ${name}!</div>`;
+
+// This renders <div>Hello Steve!</div> to the document body
+render(helloTemplate('Steve'), document.body);
+
+// This updates to <div>Hello Kevin!</div>, but only updates the ${name} part
+render(helloTemplate('Kevin'), document.body);
